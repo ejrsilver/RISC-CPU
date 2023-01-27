6c17036363db8ca4f498d0f73d9cc5c62457147f
Ethan Silver <ejrsilver@gmail.com>
2023-01-27 16:30:42 -0500

    Create not_32.v
    Created bitwise not.

---
+++ not_32.v
@@ -0,0 +1,13 @@
+module not_32(
+  input [31:0] A, 
+  output [31:0] Z
+);
+  
+  genvar i;
+  
+  generate
+    for (i = 0; i < 32; i=i+1) begin
+      assign Z[i] = !A[i];
+    end
+  endgenerate
+endmodule
