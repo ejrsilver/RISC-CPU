8ed3558ba1457a32479db81548144990e2ef3a1d
Ethan Silver <ejrsilver@gmail.com>
2023-01-27 15:21:51 -0500

    Create reg_32bit.v
    Created Register template module

---
+++ reg_32bit.v
@@ -0,0 +1,12 @@
+module reg_32bit(input wire [31:0] D, input wire clk, reset, enable, output reg [31:0] q);
+  initial q = 0;
+  
+  always @(posedge clk) begin
+    if (reset) begin
+      q[31:0] = 32'b0;
+    end
+    else if (enable) begin
+      q[31:0] = D[31:0];
+    end
+  end
+endmodule
