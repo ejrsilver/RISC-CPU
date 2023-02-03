14784e2648e51bdbcab65b2a26e91fed197061c9
ejrsilver <ejrsilver@gmail.com>
2023-02-03 14:53:04 -0500

    Created and testbench and rotate right

---
+++ and_32_tb.v
@@ -0,0 +1,15 @@
+`timescale 1ns/10ps
+
+module and_tb;
+
+	reg[31:0] input_a, input_b;
+	wire[31:0] and_result;
+
+	and_32 and_instance(input_a, input_b, and_result);
+
+	initial
+		begin
+			input_a <= 2;
+			input_b <= 3;
+		end
+endmodule
