725354ddd737d75495d6b16cf8481d1668dbf327
ejrsilver <ejrsilver@gmail.com>
2023-02-23 22:51:31 -0500

    Updated divide to hopefully work.

--- div_32.v
+++ div_32.v
@@ -1,23 +1,29 @@
-module div_32(input [31:0] divisor, dividend, output reg [31:0] q, r);
-  reg [63:0] A;
-  A[31:0] = dividend;
-  
-  genvar i;
-  generate 
-    for(int i = 0;i < 32;i = i + 1) begin
-      A = A<<1;
-      A[63:32] = A[63:32] - divisor;
-      if(A[63:32]<0) 
-        begin 
-          A[0] = 0;
-          A[63:32] = A[63:32] + divisor;
-        end
-      if(A[63:32] > 0 || A[63:32] == 0)
+module div_32(input [31:0] divisor, dividend, output [31:0] q, r);
+	reg [31:0] m, q;
+    reg [63:0] a;
+	
+	integer i;
+	always @(*) begin
         begin
-          A[0] = 1;
+            q = dividend;
+            m = divisor;
+            a = 0;
+            
+            for(int i = 0;i < 32;i = i + 1) begin
+                a = {a[31:0],q[31]};
+                q = q<<1;
+                a = a - m;
+
+                if(a[31]==1) begin 
+					q[0] = 0;
+					a = a + m;
+				end
+                else begin
+					q[0] = 1;
+				end
+            end
         end
-    end
-   endgenerate
-  r = A[63:32];
-  q = A[31:0];
+	end
+	r = A[63:32];
+	q = A[31:0];
 endmodule
