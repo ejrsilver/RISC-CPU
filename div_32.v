cdd0aa03333e1a1f6c159826b5c414f9153911de
ejrsilver <ejrsilver@gmail.com>
2023-02-23 22:59:13 -0500

    Added temporary divider

--- div_32.v
+++ div_32.v
@@ -15,7 +15,12 @@ module div_32(input [31:0] divisor, dividend, output reg [31:0] q, r);
       if(A[63:32] > 0 || A[63:32] == 0)
         begin
           A[0] = 1;
+          A[0] = 1;
         end
+    end
+   endgenerate
+  r = A[63:32];
+  q = A[31:0];
     end
    endgenerate
   r = A[63:32];
