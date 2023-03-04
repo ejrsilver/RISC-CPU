7e030131dcbebc3f51a9e8132917ff00bfe60256
ejrsilver <ejrsilver@gmail.com>
2023-03-04 12:47:12 -0500

    Updated divider to match main

--- div_32.v
+++ div_32.v
@@ -1,29 +1,27 @@
-module div_32(input [31:0] divisor, dividend, output [31:0] q, r);
+module div_32(input [31:0] dividend, divisor, output [31:0] quot, rem);
 	reg [31:0] m, q;
-    reg [63:0] a;
+    reg [32:0] a;
 	
 	integer i;
 	always @(*) begin
-        begin
             q = dividend;
             m = divisor;
             a = 0;
-            
-            for(int i = 0;i < 32;i = i + 1) begin
-                a = {a[31:0],q[31]};
-                q = q<<1;
-                a = a - m;
 
-                if(a[31]==1) begin 
+            for (i = 0;i < 32;i = i + 1) begin
+                a = {a[32:1], q[31]};
+				q[31:1] = q[30:0];
+				a = a - m;
+
+                if(a[31]==1) begin
 					q[0] = 0;
-					a = a + m;
+                    a = a + m;
 				end
                 else begin
 					q[0] = 1;
 				end
             end
         end
-	end
-	r = A[63:32];
-	q = A[31:0];
+	assign quot = a[31:0];
+	assign rem = q;
 endmodule
