b94d6b0a22cdb5809eac4cc57d9b8c8eea5aba98
ejrsilver <ejrsilver@gmail.com>
2023-03-10 20:26:45 -0500

    Changed divider to maybe work

--- div_32.v
+++ div_32.v
@@ -1,40 +1,48 @@
 module div_32(input [31:0] dividend, divisor, output [31:0] quotient, remainder);
-	reg [31:0] r,q,temp;
+	reg [31:0] m,q;
+	reg [32:0] temp;
 	integer i;
 	
 	always @(*)
 	begin
-		//initialize
-		q=dividend;
-		r=0;
+		// initialize
+		temp = 0;
+		q = dividend;
+		m = divisor;
 		
-		//32 cycles for 32 bits
-		for(i = 0; i<32; i = i+1) 
-		begin
+		// 32 cycles for 32 bits
+		for(i = 0; i<32; i = i+1) begin
 			
-			//shift left both variables, q's leftmost bit gets carried into r's rightmost after shift
-			r = (r << 1);
-			r = r | q[31];
-			q = q << 1;
+			// shift left both variables, q's leftmost bit gets carried into r's rightmost after shift
+			temp = temp<<1;
+			temp[0] = q[31];
+			q = q<<1;
+
+			// sub m from temp
+			temp = temp-m;
 			
-			//if r is negative add divisor, if positive subtract
-			if(r[31]) temp = divisor;
-			else temp = ~divisor+1;
-			//add or subtract is executed accordingly
-			r = r+temp;
+			// temp negative add m, positive q[0]=0 and restore
+			if (temp[31]==1) begin
+				temp = temp + m;
+				q[0] = 0;
+			end else begin
+				q[0] = 1;
+			end
+			// //add or subtract is executed accordingly
+			// r = r+temp;
 			
-			//check if r is negative. if positive rightmost bit of q set to 1
-			if(r[31]) q = q | 0;
-			else q = q | 1;
+			// //check if r is negative. if positive rightmost bit of q set to 1
+			// if(r[31]) q = q | 0;
+			// else q = q | 1;
 			
 		end
 		
-		// if r negative restore
-		if(r[31])
-			r=r+divisor;
+		// // if r negative restore
+		// if(r[31])
+		// 	r=r+divisor;
 	end
 	assign quotient = q;
-	assign remainder = r;
+	assign remainder = temp[31:0];
 endmodule
 	
 	
